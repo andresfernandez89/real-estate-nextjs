d95fc290dc6fb62c204f56434f6fca827d485a1e
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-08-25 14:44:32 -0300

    fix: configure Edge Runtime for dynamic routes on page property

--- src/app/properties/(property)/[page]/page.tsx
+++ src/app/properties/(property)/[page]/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+export const runtime = "edge";
 import { Error404 } from "@/components/Error404";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
